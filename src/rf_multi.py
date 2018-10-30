e502c55e6a52c85d10ed016c3cf4b35e1cf76f4d
jrasero <jrasero.daparte@gmail.com>
2018-10-30 16:56:57 +0100

    Update rf_multi.py
    New code including internal hyperparameter grid search

--- src/rf_multi.py
+++ src/rf_multi.py
@@ -1,9 +1,15 @@
-fold_model = "./results/rf_multi/"
+fold_model = "./results/rf_multi_grid/"
 
 if not os.path.exists(fold_model):
     os.makedirs(fold_model)
     
-clf = ensemble.RandomForestClassifier(class_weight= 'balanced', n_estimators= 500, n_jobs=2, verbose = 1)
+#Create the grid for hyperparameters
+min_samp_splits = [2, 10, 50, 100]
+max_features = [0.1, 0.25]
+param_grid = model_selection.ParameterGrid({'min_samp': min_samp_splits, 
+                                            'features': max_features})
+
+np.save(fold_model + "param_grid.npy", param_grid)
 
 accs = []
 for j in range(rkf.get_n_splits()):
@@ -27,22 +33,31 @@ for j in range(rkf.get_n_splits()):
     Y_true=np.argmax(Y_test,1)
     Yt=np.argmax(Y_train,1)
     
-    feats = [np.zeros(268)]
     
-    clf.fit(X_train, Yt)
+    df = pd.DataFrame(data={'true':Y_true})  
+        
+    for grid_id, hyp_param in enumerate(param_grid): 
+        
+        clf = ensemble.RandomForestClassifier(class_weight= 'balanced', 
+                                      n_estimators= 500,
+                                      min_samples_split = hyp_param['min_samp'],
+                                      max_features= hyp_param['features'],
+                                      n_jobs=-1, verbose = 1,
+                                      random_state=0)
+
+        clf.fit(X_train, Yt)
+        
+        yPred = clf.predict(X_test)
     
-    feats = np.append(feats,[clf.feature_importances_], axis=0)
-    
-    # save feature importances
-    io.savemat(fold_model +'features_fold_' + str(j)+ '.mat', {'features': feats})
-    
-    yPred = clf.predict(X_test)
-    acc = metrics.accuracy_score(Y_true, yPred)
-    accs.append(acc) 
-    
-    df = pd.DataFrame(data={'true':Y_true ,'pred': yPred})
-    
-    #Save predictions
-    df.to_csv(fold_model+'res_fold_' +str(j) + '.csv', index = False)
+        
+        pred_col = 'pred_grid_' + str(grid_id)  
+        
+        df = pd.concat([df, pd.DataFrame({pred_col: yPred})], axis=1) 
+        
+        print('Fold ', j, ' with max_depth ', str(hyp_param['min_samp']),
+              'and max_fetures ', hyp_param['features'], ' completed ')
+            
     print('Fold= ', j, ' completed ' )
-    j = j + 1
+        #j = j + 1
+     
+    df.to_csv(fold_model+'res_fold_' +str(j) + '.csv', index = False)
